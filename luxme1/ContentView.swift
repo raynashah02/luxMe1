3c97ea26d8f2992d48d5af8d1f77502a090c737f
raynashah02 <108150607+raynashah02@users.noreply.github.com>
2023-03-03 09:31:30 -0600

    Initial Commit

---
+++ luxme1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  luxme1
+//
+//  Created by Rayna Shah on 3/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ luxme1/luxme1App.swift
@@ -0,0 +1,17 @@
+//
+//  luxme1App.swift
+//  luxme1
+//
+//  Created by Rayna Shah on 3/3/23.
+//
+
+import SwiftUI
+
+@main
+struct luxme1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
